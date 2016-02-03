b85fc190ca6202354026f21f5679be1bb4fef2f6
Olli Vanhapiha <olli.vanhapiha@gmail.com>
2016-02-03 12:41:45 +0200

    Release 0.1.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.sharetribe/aws-sig4 "0.1.1-SNAPSHOT"
+(defproject org.sharetribe/aws-sig4 "0.1.1"
   :description "Middleware to add AWS signature v4 signing to clj-http requests."
   :url "http://github.com/sharetribe/aws-sig4"
   :license {:name "Apache License, Version 2.0"
