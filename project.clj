695b479ea4b4f1642a3fabbba6c6b66f9671021a
Olli Vanhapiha <olli.vanhapiha@gmail.com>
2016-02-03 12:27:08 +0200

    Update dependencies, bump version to 0.1.1-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,11 +1,11 @@
-(defproject org.sharetribe/aws-sig4 "0.1.0-alpha1"
+(defproject org.sharetribe/aws-sig4 "0.1.1-SNAPSHOT"
   :description "Middleware to add AWS signature v4 signing to clj-http requests."
   :url "http://github.com/sharetribe/aws-sig4"
   :license {:name "Apache License, Version 2.0"
             :url "http://www.apache.org/licenses/LICENSE-2.0"}
   :dependencies [[org.clojure/clojure "1.7.0"]
-                 [clj-http "2.0.0"]
+                 [clj-http "2.0.1"]
                  [pathetic "0.5.1"]
-                 [buddy/buddy-core "0.8.1"]
+                 [buddy/buddy-core "0.9.0"]
                  [clj-time "0.11.0"]]
   :profiles {:dev {:dependencies [[criterium "0.4.3"]]}})
